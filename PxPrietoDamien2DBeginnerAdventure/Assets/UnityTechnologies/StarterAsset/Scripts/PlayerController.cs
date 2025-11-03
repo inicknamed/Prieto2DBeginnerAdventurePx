e2f689d7b7e6a77f5d080cb56f866a67685f4919
Damien <756225@student.dusd.net>
2025-11-03 15:24:30 -0800

    Continued Steps
    Followed steps and went through until page 3.

--- PxPrietoDamien2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ PxPrietoDamien2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,5 +20,13 @@ public class PlayerController : MonoBehaviour
         Vector2 Positio = transform.position;
         Positio.y = Positio.y + 0.01f;
         transform.position = Positio;
+        float horizontal = 0.0f;
+        if (Keyboard.current.aKey.isPressed)
+        {
+        horizontal = -1.0f;
+            if (Keyboard.current.dKey.isPressed)
+            {
+        horizontal = 1.0f;
+        }
     }
 }
