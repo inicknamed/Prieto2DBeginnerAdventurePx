ab3d0fd2b97293ef51c8181567599b0fe4d286cc
Damien <756225@student.dusd.net>
2025-11-12 15:27:08 -0800

    Followed Steps (completed 2 pgs)
    Followed through steps, got to create decorative objects step one.
    *Needs to finish doing tile pallete

--- PxPrietoDamien2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ PxPrietoDamien2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -4,57 +4,21 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-    
 {
-    public InputAction LeftAction;
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //---------------------------------------------- 
-        float horizontal = 0.0f;
-        float vertical = 0.0f;
-     
-
-        //tickle
-        //keys
-        if (Keyboard.current.aKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        if (Keyboard.current.dKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-
-        else if (Keyboard.current.dKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-        if (Keyboard.current.sKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        if (Keyboard.current.wKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-
-
-        Debug.Log(horizontal);
-        //--------------------------------
-
-        Vector2 Position = transform.position;
-        Position.x = Position.x + 0.01f * horizontal;
-        transform.position = Position;
-        Vector2 Positio = transform.position;
-        Positio.y = Positio.y + 0.01f * vertical;
-        transform.position = Positio;
-        //
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+        transform.position = position;
     }
 }
